7dea46cfd1f1538f830550bed0ff701d25e32e63
Keneth Ernesto Macis Flores <kmacis@unan.edu.ni>
2025-03-31 17:20:40 -0600

    Vistas de Gestión de Archivos

---
+++ modulo_admin/Controllers/ArchivoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace modulo_admin.Controllers
+{
+    public class ArchivoController : Controller
+    {
+        // GET: Archivo
+        public ActionResult GestionArchivos()
+        {
+            return View();
+        }public ActionResult CarpetasCompartidas()
+        {
+            return View();
+        }public ActionResult ArchivosCompartidos()
+        {
+            return View();
+        }
+    }
+}
