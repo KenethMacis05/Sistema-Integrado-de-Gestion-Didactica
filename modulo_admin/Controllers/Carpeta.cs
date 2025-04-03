ff71945165aded6fdfee0280a91b1d332704681a
keneth Ernesto Macis Flores <kenethmacis050802@gmail.com>
2025-04-02 23:55:19 -0600

    Nose

---
+++ modulo_admin/Controllers/Carpeta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace modulo_admin.Controllers
+{
+    public class Carpeta : Controller
+    {
+        // GET: Carpeta
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
